1b891116fa21d849ac834c591258dc8dff318cbb
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-04-29 18:31:12 +0700

    mengisi void

--- introduceOop.cpp
+++ introduceOop.cpp
@@ -6,6 +6,12 @@ class Mahasiswa{
     int nim;
     string nama;
     float nilai;
+
+    void printdata(){
+        cout << "nim : " << nim << endl;
+        cout << "nama : " << nama << endl;
+        cout << "nilai : " << nilai << endl;
+    }
 };
 
 int main(){
